cd190f920de49547035db43f7788e629dd37cdad
Johan Andersson <johan.rejeep@gmail.com>
2009-04-30 18:52:50 +0000

    Added "method missing" example.

--- example/GameController.java
+++ example/GameController.java
@@ -6,10 +6,19 @@
 public class GameController extends ApplicationController
 {
   /**
-   * Is called when the user saves.
+   * Is called when the user clicks the save button.
    */
   public void save()
   {
-    System.out.println( "Saving the game..." );
+    System.out.println("Saving the game...");
+  }
+
+  /**
+   * Is called when the user click the random button.
+   */
+  public void methodMissing()
+  {
+    // The variable name is set in ActionController.
+    System.out.println("Random: " + name);
   }
 }
--- example/Main.java
+++ example/Main.java
@@ -1,6 +1,7 @@
-import javax.swing.JFrame;
-import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.util.Random;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 
 /**
  * Main class.
@@ -11,15 +12,27 @@ public class Main extends JFrame
 {
   public Main()
   {
-    setLayout(new BorderLayout());
+    setLayout(new FlowLayout());
     
     GameController gameController = new GameController();
 
+    // Save button that show the default usage.
     JButton save = new JButton("Click here to save the Game!");
     save.addActionListener(gameController);
     save.setName("save");
     add(save);
-    
+
+    // Shows how to use the "method missing" functionality.
+    Random generator = new Random();
+    String[] fruits = { "apple", "apricot", "orange", "pear", "plum", "peach", "watermelon" };
+    int index = generator.nextInt(fruits.length);
+    String randomName = fruits[index];
+
+    JButton random = new JButton("Click here to show what random picked!");
+    random.addActionListener(gameController);
+    random.setName(randomName);
+    add(random);
+
     pack();
     setVisible(true);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
