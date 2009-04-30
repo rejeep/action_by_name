1f6821770aee1cbce9a1e2e6319bded3becbe45a
Johan Andersson <johan.rejeep@gmail.com>
2009-04-30 18:41:24 +0000

    Controller is now GameController.

--- example/GameController.java
+++ example/GameController.java
@@ -1,15 +1,15 @@
 /**
- * Controller class.
+ * Game controller class.
  *
  * @author Johan Andersson (johan.rejeep@gmail.com)
  */
-public class Controller extends ApplicationController
+public class GameController extends ApplicationController
 {
   /**
    * Is called when the user saves.
    */
   public void save()
   {
-    // Save...
+    System.out.println( "Saving the game..." );
   }
 }
